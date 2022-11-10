e85dcdecea0089a93fce2c64b4c7c35af0c28ade
Mohammad Mahdi Baghbani Pourvahid <mahdi-baghbani@azadehafzar.ir>
2022-11-10 19:18:58 +0330

    add: supervisor will run horizon inside image

--- cyb-app/docker-entrypoint.sh
+++ cyb-app/docker-entrypoint.sh
@@ -10,10 +10,21 @@ cd /app || exit
 # clean install packages.
 composer install --no-interaction --optimize-autoloader --no-dev
 
-# php horizon create assets.
-php artisan horizon:install --no-interaction
-php artisan horizon:publish --no-interaction
-
+# run database migrations prior to launching app.
 php artisan migrate --force
+
+# php horizon create assets.
+php artisan horizon:install --no-interaction  --quiet
+php artisan horizon:publish --no-interaction  --quiet
+
+# run horizon via supervisor.
+# nohup and the end of line & will push supervisord to background
+nohup supervisord &
+
+# wait until supervisor is ready.
+sleep 2
+
+# run app.
+# TODO: should we read $PORT environment variable and pass it into artisan serv via --port ?
 php artisan optimize
 php artisan serv --host=0.0.0.0
