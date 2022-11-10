63b3ad11d38a775e09f9baf526c9c694b0832bdc
Mohammad Mahdi Baghbani Pourvahid <mahdi-baghbani@azadehafzar.ir>
2022-11-10 16:28:13 +0330

    modify: add horizon publish to entrypoint

--- cyb-app/docker-entrypoint.sh
+++ cyb-app/docker-entrypoint.sh
@@ -2,6 +2,13 @@
 
 cd /app || exit
 
+# clean install packages.
+RUN composer install --no-interaction --optimize-autoloader --no-dev
+
+# php horizon create assets.
+RUN php artisan horizon:install --no-interaction
+RUN php artisan horizon:publish --no-interaction
+
 php artisan migrate --force
 php artisan optimize
 php artisan serv --host=0.0.0.0
