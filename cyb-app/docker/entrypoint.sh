8c5d7093107dd629e2d5657e2e6d4afd2f05520f
Mohammad Mahdi Baghbani Pourvahid <mahdi-baghbani@azadehafzar.ir>
2022-11-11 15:45:25 +0330

    add: new docker-compose file

--- cyb-app/docker/entrypoint.sh
+++ cyb-app/docker/entrypoint.sh
@@ -10,21 +10,12 @@ if [ ! -d /.composer ]; then
     mkdir /.composer
 fi
 
-chmod -R ugo+rw /.composer
+chmod --recursive ugo+rw /.composer
 
 if [ $# -gt 0 ]; then
     exec gosu "${WWWUSER}" "$@"
 else
-    if [[ "${APP_ENTRY_MODE}" == "artisan" ]]; then
-        # install packages.
-        /usr/bin/composer install --no-interaction --optimize-autoloader --no-dev
-        # run database migrations prior to launching app.
-        /usr/bin/php -d variables_order=EGPCS /var/www/html/artisan migrate --force
-        # optimize app sources.
-        /usr/bin/php -d variables_order=EGPCS /var/www/html/artisan optimize
-        # serve application.
-        exec /usr/bin/php -d variables_order=EGPCS /var/www/html/artisan serve --host=0.0.0.0 --port=8000
-    elif [[ "${APP_ENTRY_MODE}" == "horizon" ]]; then
+    if [[ "${APP_ENTRY_MODE}" == "horizon" ]]; then
         # wait until main container is fully up and running.
         sleep 10
         # publish horizon assets into /var/www/html/public/vendor/horizon .
@@ -33,13 +24,25 @@ else
         # start horizon.
         exec /usr/bin/php -d variables_order=EGPCS /var/www/html/artisan horizon
     else
+        # populate /var/www/html with cyb app.
+        cp --archive --recursive --no-clobber ~/CYB-dockerization/cyb-app/. /var/www/html
+
+        # set file ownership.
+        chown --recursive "${WWWUSER}":"${WWWUSER}" /var/www/html
+
         # install packages.
         /usr/bin/composer install --no-interaction --optimize-autoloader --no-dev
         # run database migrations prior to launching app.
         /usr/bin/php -d variables_order=EGPCS /var/www/html/artisan migrate --force
         # optimize app sources.
         /usr/bin/php -d variables_order=EGPCS /var/www/html/artisan optimize
-        # start fpm.
-        exec /etc/init.d/php8.1-fpm start
+
+        if [[ "${APP_ENTRY_MODE}" == "artisan" ]]; then
+            # serve application.
+            exec /usr/bin/php -d variables_order=EGPCS /var/www/html/artisan serve --host=0.0.0.0 --port=8000
+        else
+            # start fpm.
+            exec /etc/init.d/php8.1-fpm start
+        fi
     fi
 fi
