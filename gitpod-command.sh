24dabfcb5bd28e7c89c7a7b3088d746319d341e9
Mohammad Mahdi Baghbani Pourvahid <mahdi-baghbani@azadehafzar.ir>
2022-11-12 20:40:55 +0330

    add: docker.sh to handle prod | dev | gitpod environment setups

--- gitpod-command.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-cd cyb-app
-docker compose --file docker-compose.gitpod.yaml build
-docker compose --file docker-compose.gitpod.yaml pull
-docker compose --file docker-compose.gitpod.yaml up --detach
