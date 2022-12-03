0358d9cda3b5f3fb1e19972ed4edd0df7373c8ce
Mohammad Mahdi Baghbani Pourvahid <mahdi-baghbani@azadehafzar.ir>
2022-12-03 15:38:38 +0330

    fix: database scheme

--- cyb-app/database/migrations/2022_11_30_122920_create_recurring_tasks.php
+++ cyb-app/database/migrations/2022_11_30_122920_create_recurring_tasks.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('recurring_tasks', function (Blueprint $table) {
             $table->id();
-            $table->integer('interval');
+            $table->string('interval');
             $table->string('function');
             $table->string('parameters');
             $table->timestamps();
