bb8beda539fd4d3a8d4659f94c6f916d284db9e7
Yashar PourMohammad <yasharpm@gmail.com>
2023-01-30 12:18:42 +0000

    Added constants for acube

---
+++ cyb-app/app/Connectors/LetsPeppol/ACube/Constants.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Connectors\LetsPeppol\ACube;
+
+class Constants
+{
+
+    public const SANDBOX_BASE_URL = 'https://peppol-sandbox.api.acubeapi.co';
+    public const SANDBOX_LOGIN_URL = 'https://common-sandbox.api.acubeapi.com/login';
+
+    public const PRODUCTION_BASE_URL = 'https://peppol.api.acubeapi.com';
+    public const PRODUCTION_LOGIN_URL = 'https://common.api.acubeapi.com/login';
+
+    public const USE_SANDBOX = true;
+
+    public const BASE_URL = self::USE_SANDBOX ? self::SANDBOX_BASE_URL : self::PRODUCTION_BASE_URL;
+    public const LOGIN_URL = self::USE_SANDBOX ? self::SANDBOX_LOGIN_URL : self::PRODUCTION_LOGIN_URL;
+
+    public const USERNAME = self::USE_SANDBOX ? Secrets::SANDBOX_USERNAME : Secrets::PRODUCTION_USERNAME;
+    public const PASSWORD = self::USE_SANDBOX ? Secrets::SANDBOX_PASSWORD : Secrets::PRODUCTION_PASSWORD;
+
+}
---
+++ cyb-app/app/Connectors/LetsPeppol/ACube/Secrets.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Connectors\LetsPeppol\ACube;
+
+class Secrets
+{
+
+    public const SANDBOX_USERNAME = '';
+    public const SANDBOX_PASSWORD = '';
+
+    public const PRODUCTION_USERNAME = '';
+    public const PRODUCTION_PASSWORD = '';
+}
