ec99780b44bab6e974fc1dea7d24b2a5666af24f
Yashar PourMohammad <yasharpm@gmail.com>
2023-02-01 22:03:10 +0000

    Set 503 response code when webhook handling fails

--- cyb-app/app/Connectors/LetsPeppol/routes/api.php
+++ cyb-app/app/Connectors/LetsPeppol/routes/api.php
@@ -5,15 +5,23 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::name('acube-incoming')->post('/acube/incoming', function (Request $request) {
-    $service = new LetsPeppolService();
-    $service->newMessage(LetsPeppolService::REGISTRAR_ACUBE, true, $request->all());
+    try {
+        $service = new LetsPeppolService();
+        $service->newMessage(LetsPeppolService::REGISTRAR_ACUBE, true, $request->all());
 
-    return ['message' => 'Thank you for sharing!'];
+        return ['message' => 'Thank you for sharing!'];
+    } catch (\Exception $e) {
+        abort(503); // So that ACube would retry later!
+    }
 });
 
 Route::name('acube-outgoing')->post('/acube/outgoing', function (Request $request) {
-    $service = new LetsPeppolService();
-    $service->newMessage(LetsPeppolService::REGISTRAR_ACUBE, false, $request->all());
+    try {
+        $service = new LetsPeppolService();
+        $service->newMessage(LetsPeppolService::REGISTRAR_ACUBE, false, $request->all());
 
-    return ['message' => 'Thank you for sharing!'];
+        return ['message' => 'Thank you for sharing!'];
+    } catch (\Exception $e) {
+        abort(503); // So that ACube would retry later!
+    }
 });
